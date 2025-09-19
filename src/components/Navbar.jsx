06c18b5b5c86fd53fd47d9706d2db84bd618de39
Shreyanjain07 <shreyansjain2337@gmail.com>
2025-09-20 02:57:52 +0530

    Updated Favicon and Website title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import logo from "../assets/FinalName.png";
 
 function Navbar() {
   return (
     <header className="bg-white shadow">
-      <div className="container mx-auto px-6 py-4 flex justify-between items-center">
-        <h1 className="text-3xl font-bold text-green-700">Travora</h1>
+      <div className="container mx-auto px-6 py-2 flex justify-between items-center">
+        {/* <h1 className="text-3xl font-bold text-green-700">Travora</h1> */}
+        <img src={logo}  alt="Travora" className="h-18 w-32"/>
         <nav className="space-x-6">
           <a href="/" className="hover:text-violet-600 font-medium">
             Home
